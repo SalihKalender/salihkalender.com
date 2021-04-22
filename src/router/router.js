6c2577b23bffc0b4fbf89bffff2854c2b3e38f45
SalihKalender <67439743+SalihKalender@users.noreply.github.com>
2021-04-22 16:36:36 +0300

    Update router.js

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-// import Header from './components/header' //TODO: Bunları bi dene ozaman
-// import Loading from './components/loading'
+
 import HomePage from '../components/homepage'
 import Experiences from '../components/experiences'
 import Projects from '../components/projects'
@@ -11,12 +10,11 @@ import Contact from '../components/contact'
 
 Vue.use(VueRouter);
 
-const routes = [    //* route degistigi zaman zaman zaten router-view degisecegi icin sadece app.vue 'u tanımlamana ve kullanmana gerek yok, aynen, header'ıda name'li router-view yapman lazım
+const routes = [  
     { path: '/' , component: HomePage , name: 'HomePage'},
     { path: '/projeler' , component: Projects , name: 'Projects' },
     { path: '/tecrubeler' , component: Experiences , name: 'Experiences' },
     { path: '/iletisim' , component: Contact , name: 'Contact' }
-    //TODO: BIRDE 404 SAYFASI VAR
 ]
 
 export const router = new VueRouter({
